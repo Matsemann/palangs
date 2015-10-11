3e038e4d9fc04bf83b280d5900268fb83ce8ffcd
Mats Krüger Svensson <mats@matsemann.com>
2015-10-11 16:24:02 +0200

    gulp builds the frontend

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,58 @@
 var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+var concat = require('gulp-concat');
+var clean = require('gulp-clean');
 
 var dest = 'static';
 var src = 'frontend';
 
+var toCopy = [
+    src + "/index.html",
+    src + "/styles.css"
+];
+
+var vendors = [
+    "node_modules/svg-pan-zoom/dist/svg-pan-zoom.min.js"
+];
+
+gulp.task('clean', function () {
+    return gulp.src(dest, {read: false})
+        .pipe(clean());
+});
+
 gulp.task('copy', function () {
-    gulp.src(src + "/world/*")
+    return gulp.src(toCopy)
+        .pipe(gulp.dest(dest));
+});
+
+gulp.task('copy-world', function () {
+    return gulp.src(src + "/world/*")
         .pipe(gulp.dest(dest + "/world"));
 });
 
-gulp.task('default', function() {
-    // place code for your default task here
+gulp.task('js', function () {
+    return gulp.src(src + "/**/*.js")
+        .pipe(sourcemaps.init())
+        .pipe(concat('app.js'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(dest + "/js"));
 });
+
+gulp.task('js-vendors', function () {
+    return gulp.src(vendors)
+        .pipe(concat('vendors.js'))
+        .pipe(gulp.dest(dest + "/js"));
+});
+
+gulp.task('default', ["clean"], function() {
+    gulp.run(["copy", "copy-world", "js", "js-vendors"]);
+});
+
+gulp.task('watch', ["clean"], function() {
+    gulp.run(["copy", "copy-world", "js", "js-vendors"]);
+
+    gulp.watch(src + "/**/*.js", ["js"]);
+    gulp.watch(toCopy, ["copy"]);
+    gulp.watch(src + "/world/*2", ["copy-world"]);
+});
+
